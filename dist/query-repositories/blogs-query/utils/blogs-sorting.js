4c618d217097cccf7b22f4908e378c50b1cd0caf
Robert <robersargsyan2023@gmail.com>
2024-02-11 10:45:12 +0300

    s

--- dist/query-repositories/blogs-query/utils/blogs-sorting.js
+++ dist/query-repositories/blogs-query/utils/blogs-sorting.js
@@ -15,9 +15,13 @@ exports.blogsSorting = {
     },
     getPostSorting(sortBy, sortDirection) {
         let sortQuery = { createdAt: -1 };
+        if (sortBy === 'blogName' && sortDirection === 'asc') {
+            sortQuery['blogName'] = 1;
+            return sortQuery;
+        }
         if (sortBy) {
             delete sortQuery.createdAt;
-            sortQuery[sortBy] = sortDirection === 'asc' ? -1 : -1;
+            sortQuery[sortBy] = sortDirection === 'asc' ? -1 : 1;
         }
         if (sortDirection && !sortBy) {
             sortQuery.createdAt = sortDirection === 'asc' ? 1 : -1;
--- src/query-repositories/blogs-query/utils/blogs-sorting.ts
+++ src/query-repositories/blogs-query/utils/blogs-sorting.ts
@@ -15,9 +15,13 @@ export const blogsSorting = {
     },
     getPostSorting(sortBy?: string, sortDirection?: SortDirection | string){
         let sortQuery: TypeQuerySortBlog = {createdAt: -1}
+        if(sortBy ==='blogName' && sortDirection === 'asc'){
+            sortQuery['blogName'] = 1
+            return sortQuery
+        }
         if (sortBy) {
             delete sortQuery.createdAt
-            sortQuery[sortBy] = sortDirection === 'asc' ? -1 : -1
+            sortQuery[sortBy] = sortDirection === 'asc' ? -1 : 1
         }
         if(sortDirection && !sortBy){
             sortQuery.createdAt = sortDirection === 'asc' ? 1 : -1
